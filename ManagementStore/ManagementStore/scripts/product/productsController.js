57d52a50167f1a61f0dcc283ab5b9e680422afea
Milea Violeta <mileavioleta@outlook.com>
2016-02-14 16:33:14 +0200

    Modify Product - Modal

--- ManagementStore/ManagementStore/scripts/product/productsController.js
+++ ManagementStore/ManagementStore/scripts/product/productsController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-angular.module('ManagmentStore').controller('ProductsController', function ($scope, $location, ProductFactory) {
+angular.module('ManagmentStore').controller('ProductsController', function ($scope, $modal, $location, ProductService, ProductFactory) {
 
     $scope.products = ProductFactory.getProducts();
 
@@ -10,4 +10,23 @@ angular.module('ManagmentStore').controller('ProductsController', function ($sco
         });
     };
 
+
+    $scope.modifyProduct = function (product) {
+        ProductService.product = product;
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'myModalContent.html',
+            controller: 'ProductController',
+            size: 'md'
+        });
+    }
+
+});
+
+angular.module('ManagmentStore').controller('ProductController', function ($scope, $modalInstance, ProductService, ProductFactory) {
+    $scope.selectedProduct = ProductService.product;
+});
+
+angular.module('ManagmentStore').service('ProductService', function () {
+    var product;
 });
